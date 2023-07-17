3637bed3b6070650d1d2e66b86f95706e4db648a
Crowdbotics <team@crowdbotics.com>
2023-07-17 23:49:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "isignal-42235",
+  url: "https://isignal-42235.botics.co",
+  api: "https://isignal-42235.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
